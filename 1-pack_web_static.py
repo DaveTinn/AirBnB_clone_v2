ea0731c20cca0c688b739a6343ececc6f86fde98
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-04-05 04:02:02 -0700

    Generates a .tgz archive from the contents of web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,7 +16,7 @@ def do_pack():
     timenow = strftime("%Y%M%d%H$M$S")
     try:
         local("mkdir -p versions")
-        archname = "versions/wbe_static_{}.tgz".format(timenow)
+        archname = "versions/web_static_{}.tgz".format(timenow)
         local("tar -cvzf {} web_static/".format(archname))
         return archname
     except:
